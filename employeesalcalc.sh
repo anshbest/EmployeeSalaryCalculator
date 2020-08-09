dfe9c7a61f0adf41e0654fee1876157e933201c9
anshbest <anshulmnc123@gmail.com>
2020-08-09 05:41:32 +0530

    calculating daywise wage storing days in dictionary

--- employeesalcalc.sh
+++ employeesalcalc.sh
@@ -4,13 +4,13 @@ IS_PART_TIME=1
 IS_FULL_TIME=2
 MAX_HRS_IN_MONTH=100
 NUM_WORKING_DAYS=20
-EMP_RATE_PER_HR=20
-NUM_WORKING_DAYS=20
+EMP_RATE_PER_HR=25
+NUM_WORKING_DAYS=40
 
 totalWorkHours=0
 totalWorkingDays=0
 
-function getWorkingHours(){
+function getHours(){
 	case $1 in
 		$IS_FULL_TIME)
 			workHours=8
@@ -34,10 +34,11 @@ function calcDailyWage(){
 while [[ $totalWorkHours -lt $MAX_HRS_IN_MONTH && $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
 do
 	((totalWorkingDays++))
-	workHours="$( getWorkingHours $((RANDOM%3)) )"
+	workHours="$( getHours $((RANDOM%3)) )"
 	totalWorkHours=$(($totalWorkHours+$workHours))
-	empDailyWage[$totalWorkingDays]="$( calcDailyWage $workHours )"
+	empDailyWage["Day""$totalWorkingDays"]="$( calcDailyWage $workHours )"
 done
 
 totalSalary=$(($totalWorkHours*$EMP_RATE_PER_HR))
 echo "Daily Wage " ${empDailyWage[@]}
+echo " keys" ${!empDailyWage[@]}
